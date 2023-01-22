06d910d5abd868aae48152929ac6be563d04e07f
butevichanastas <butevichanastas@gmail.com>
2023-01-23 00:09:05 +0300

    feat: add const

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function toReadable (number) {
-  
+  const one = 'one';
+  const two = 'two';
+  const three = 'three';
+  const four = 'four';
+  const five = 'five';
+  const six = 'six';
+  const seven = 'seven';
+  const eight = 'eight';
+  const nine = 'nine';
+  const ten = 'ten';
+  const eleven = 'eleven';
+  const twelve = 'twelve';
+  const thirteen = 'thirteen';
+  const fourteen = 'fourteen';
+  const fifteen = 'fifteen';
+  const sixteen = 'sixteen';
+  const seventeen = 'seventeen';
+  const eighteen = 'eighteen';
+  const nineteen = 'nineteen';
+  const twenty = 'twenty';
+  const thirty = 'thirty';
+  const forty = 'forty';
+  const fifty = 'fifty';
+  const sixty = 'sixty';
+  const seventy = 'seventy';
+  const eighty = 'eighty';
+  const ninety = 'ninety';
+  const oneHundred = 'hundred';
 }
+
+333 = 3*100 + 30 + 3
+'three hundred thirty three'
+
+let str = String(Number);
+let length = str.length;
